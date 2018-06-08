2cf0ae4c3899f87f6b773651eb597ce59ff75e20
Hui Chun <pewpewt97@gmail.com>
2018-06-09 05:03:21 +0800

    Layout configuration for Login screen.

--- app/src/main/java/com/example/user/payme/LoginActivity.java
+++ app/src/main/java/com/example/user/payme/LoginActivity.java
@@ -1,13 +1,58 @@
 package com.example.user.payme;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailTxt;
+    private EditText pwdTxt;
+    private Button loginBtn;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize widgets
+        emailTxt = findViewById(R.id.emailTxtField);
+        pwdTxt = findViewById(R.id.pwdTxtField);
+        loginBtn = findViewById(R.id.loginBtn);
+        auth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailTxt.getText().toString().trim();
+                String password = pwdTxt.getText().toString().trim();
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            // End the activity
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/user/payme/SignupActivity.java
+++ app/src/main/java/com/example/user/payme/SignupActivity.java
@@ -1,15 +1,13 @@
 package com.example.user.payme;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_signup);
     }
 }
