dd1bfa6eeb7b6971b89b8704c26dbcb841efb231
Hui Chun <pewpewt97@gmail.com>
2018-06-12 05:12:49 +0800

    Email and Password authentication completed. Verification and Reset Password email feature completed.

--- app/src/main/java/com/example/user/payme/LoginActivity.java
+++ app/src/main/java/com/example/user/payme/LoginActivity.java
@@ -1,25 +1,32 @@
 package com.example.user.payme;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailTxt;
     private EditText pwdTxt;
+    private TextView forgotPwd;
     private Button loginBtn;
+    private Button signUpBtn;
     private FirebaseAuth auth;
 
     @Override
@@ -30,7 +37,9 @@ public class LoginActivity extends AppCompatActivity {
         // Initialize widgets
         emailTxt = findViewById(R.id.emailTxtField);
         pwdTxt = findViewById(R.id.pwdTxtField);
+        forgotPwd = findViewById(R.id.forgotPwdLink);
         loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
         auth = FirebaseAuth.getInstance();
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -39,18 +48,48 @@ public class LoginActivity extends AppCompatActivity {
                 String email = emailTxt.getText().toString().trim();
                 String password = pwdTxt.getText().toString().trim();
 
-                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                            // End the activity
-                            finish();
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setMessage("Email or password cannot be empty!")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    alert.show();
+                } else {
+                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                // End the activity
+                                finish();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+                }
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(signUp);
+            }
+        });
+
+        forgotPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resetPwd = new Intent(LoginActivity.this, ResetPasswordActivity.class);
+                startActivity(resetPwd);
             }
         });
     }
