b43ba5c32055e6608627380594908661feb39400
Lee Tze Ting <ting00733@gmail.com>
2018-06-12 23:50:59 +0800

    Working BottomNavigationBar with shift mode off

--- app/src/main/java/com/example/user/payme/MainActivity.java
+++ app/src/main/java/com/example/user/payme/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.user.payme;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_history:
+                    return true;
+                case R.id.navigation_addNewReceipt:
+                    Intent intent = new Intent(MainActivity.this, AddReceipt.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_contacts:
+                    return true;
+                case R.id.navigation_account:
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
